4f1d71ac38e6a7d0c2d9dd41e7b88411137bc500
xuwenbin <xuwenbin@sfere-elec.com>
2021-09-08 15:47:38 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <Line />
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Line from './components/Line.vue';
+export default defineComponent({
+    name: 'App',
+    components: {Line, HelloWorld},
+    setup() {
+        const a = ref('111');
+
+        return {
+            a,
+        };
+    },
+});
+</script>
+
+<style></style>
---
+++ src/components/Line.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="line" id="line">line</div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, onMounted} from 'vue';
+import * as echarts from 'echarts';
+export default defineComponent({
+    name: 'Line',
+    setup() {
+        const initEcharts = () => {
+            const myChart = echarts.init(document.getElementById('line'));
+            const option = {
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                },
+                yAxis: {
+                    type: 'value',
+                },
+                series: [
+                    {
+                        data: [150, 230, 224, 218, 135, 147, 260],
+                        type: 'line',
+                    },
+                ],
+            };
+
+            myChart.setOption(option);
+        };
+
+        onMounted(() => {
+            initEcharts();
+        });
+
+        return {
+            initEcharts,
+        };
+    },
+});
+</script>
+
+<style scoped>
+.line {
+    width: 800px;
+    height: 500px;
+}
+</style>
