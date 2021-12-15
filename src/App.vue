d86f414109d613ee73ba7dc06fdb069e4447d904
xuwenbin <xuwenbin@sfere-elec.com>
2021-12-15 09:14:54 +0800

    添加3d地铁

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Line />
     <AreaStack />
     <BarSimple />
+    <Map />
 </template>
 
 <script lang="ts">
@@ -11,9 +12,10 @@ import Line from './components/Line.vue';
 import AreaStack from './components/AreaStack.vue';
 import BarSimple from './components/BarSimple.vue';
 import WaterLevel from './components/WaterLevel.vue';
+import Map from './components/Map.vue';
 export default defineComponent({
     name: 'App',
-    components: {Line, AreaStack, BarSimple, WaterLevel},
+    components: {Line, AreaStack, BarSimple, WaterLevel, Map},
     setup() {},
 });
 </script>
